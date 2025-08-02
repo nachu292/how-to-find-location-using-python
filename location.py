61e05d21e192098cf8824ce4e97f76f16b34464e
Sharwanand k a <anuanoopkv@gmail.com>
2025-08-02 19:18:42 +0530

    Update location.py

--- location.py
+++ location.py
@@ -1,5 +1,5 @@
 import phonenumbers
 from phonenumbers import geocoder
-phone_number = phonenumbers.parse("+919048278833")
+phone_number = phonenumbers.parse("+195369429786")
 print("\nPhone Number Location\n")
 print(geocoder.description_for_number(phone_number, 'en'))
